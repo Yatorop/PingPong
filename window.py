ac7613af026a7922a549d3afbf6a2ffd8e91d243
Yatorop <162882173+Yatorop@users.noreply.github.com>
2024-03-17 12:56:56 +0200

    Update window.py

--- window.py
+++ window.py
@@ -1,29 +1,51 @@
-import pygame
-import sys
+from pygame import *
 
-# Ініціалізація Pygame
-pygame.init()
+window = display.set_mode((500, 700))
+display.set_caption('Пінг-Понг')
+background = transform.scale(image.load("sky.jpg"), (500, 700))
 
-# Розміри вікна
-width = 800
-height = 600
+class GameSprite(sprite.Sprite):
+    def init(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        sprite.Sprite.init(self)
+ 
+        self.image = transform.scale(
+            image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+ 
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+ 
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+ 
+class Player(GameSprite):
+    def update_up(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < 500 - 80:
+            self.rect.x += self.speed
 
-# Створення вікна
-screen = pygame.display.set_mode((width, height))
+    def update_down(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_DOWN] and self.rect.x < 500 - 80:
+            self.rect.x += self.speed
 
-# Білий колір
-WHITE = (255, 255, 255)
 
-# Головний цикл програми
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+clock = time.Clock()
+FPS = 60
 
-    # Заповнення екрана білим кольором
-    screen.fill(WHITE)
+game = True
 
-    # Оновлення відображення
-    pygame.display.flip()
+while game:
+    window.blit(background, (0, 0))
 
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+    display.update()
+    clock.tick(FPS)
