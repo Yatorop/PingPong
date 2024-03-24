969bf06500f1cc63ae91db14a358334249c8de14
Yatorop <162882173+Yatorop@users.noreply.github.com>
2024-03-24 13:23:56 +0200

    Update window.py

--- window.py
+++ window.py
@@ -31,11 +31,14 @@ class Player(GameSprite):
         keys = key.get_pressed()
         if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed_x
-
+        if keys[K_s] and self.rect.y > 5:
+            self.rect.y += self.speed_x
     def update_r(self):
         keys = key.get_pressed()
         if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed_x
+        if keys[K_DOWN] and self.rect.y > 5:
+            self.rect.y += self.speed_x
 
 class Ball(GameSprite):
     def update(self):
